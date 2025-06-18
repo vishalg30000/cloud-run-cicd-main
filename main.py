33861f9ce3891b3d7593f3a57f1ccacbb9ca7b64
vishal gupta <name@fractal.ai>
2025-06-18 15:39:29 +0000

    fixes

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ version = os.environ.get("VERSION", "dev")
 
 @app.route("/")
 def hello():
-    return f"Hello from Cloud Run Welcomes you! Version: {version}"
+    return f"Hello vishal  from Cloud Run Welcomes you! Version: {version}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
